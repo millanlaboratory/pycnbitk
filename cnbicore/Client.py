fc32badefc32eba00fb0183960824c95c43619eb
Luca Tonin <luca.tonin@gmail.com>
2017-08-20 19:37:38 +0200

    working on server and client

--- cnbicore/Client.py
+++ cnbicore/Client.py
@@ -1,31 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 import socket
 import sys
-from cnbicore import NetworkTypes
+import NetworkTypes
 
 class Client:
 
-    def __init__(self):
-        self.__sock      = None
-        self.__protocol  = None
+    def __init__(self, protocol = NetworkTypes.SOCKET_PROTOCOL_TCP):
         self.__address   = None
         self.__connected = False
-        
 
-    def Connect(self, address, port, protocol = NetworkTypes.SOCKET_PROTOCOL_TCP):
-        
-        if self.__sock:
-            return NetworkTypes.SOCKET_ERROR_BOUND
-        
-        self.__address  = (address, int(port))
         self.__protocol = protocol
-        
         if self.__protocol == NetworkTypes.SOCKET_PROTOCOL_TCP:
             self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         elif self.__protocol == NetworkTypes.SOCKET_PROTOCOL_UDP:
             self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         else:
             return NetworkTypes.SOCKET_ERROR_PROTOCOL
-       
+
+
+    def Connect(self, address, port):
+        
+        self.__address  = (address, int(port))
+              
         try:
             self.__sock.connect(self.__address)
         except socket.error:
@@ -33,13 +30,15 @@ class Client:
             return NetworkTypes.SOCKET_ERROR_CONNECTION
 
         self.__connected = True
-        return NetworkTypes.SOCKET_SUCCESSFULL
+        return NetworkTypes.SOCKET_SUCCESSFUL
 
     def Disconnect(self):
         if self.__connected == False:
             return True
 
+        self.__sock.shutdown(2)
         self.__sock.close()
+        self.__connected = False
         self.__sock = None
 
     def IsConnected(self):
@@ -50,3 +49,28 @@ class Client:
             return SOCKET_ERROR_CONNECTION
 
         self.__sock.sendall(message)
+
+
+
+if __name__ == '__main__': 
+    import sys
+    from time import sleep
+    IP = "127.0.0.1"
+    PORT = 8000
+    client = Client()
+    
+    print "Try to connect to %s:%s" % (IP, PORT)
+    while client.IsConnected() == False:
+        client.Connect(IP, PORT)
+    
+    print "Connected to %s:%s" % (IP, PORT)
+
+    sleep(5)
+    i = 0
+    while i<5:
+        client.Send('100')
+        sleep(1)
+        i = i+1
+
+    client.Disconnect()
+
--- cnbicore/Server.py
+++ cnbicore/Server.py
@@ -1,12 +1,110 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 import socket
 import sys
-from cnbicore import NetworkTypes
-
-class Client:
-    def __init__(self):
-        self.__sock      = None
-        self.__protocol  = None
+import NetworkTypes
+import select
+class Server:
+    def __init__(self, protocol = NetworkTypes.SOCKET_PROTOCOL_TCP):
         self.__address   = None
         self.__connected = False
+        self.__endpoint  = None
+        self.__endpoint_address = None
+        
+        self.__protocol = protocol
+        if self.__protocol == NetworkTypes.SOCKET_PROTOCOL_TCP:
+            self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif self.__protocol == NetworkTypes.SOCKET_PROTOCOL_UDP:
+            self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            return NetworkTypes.SOCKET_ERROR_PROTOCOL
 
+        self.__sock.setblocking(0) 
+
+    def Bind(self, address, port):
+
+        self.__address  = (address, int(port))
+             
+        try:
+            self.__sock.bind((address, int(port)))
+            self.__sock.listen(0)
+        except socket.error:
+            return False
+            
+        return True
+
+    def Accept(self):
+        try:
+            (self.__endpoint, addr) = self.__sock.accept()
+            self.__connected = True
+            self.__endpoint_address = addr
+        except socket.error:
+            return False
+
+        return True
+  
+    def GetEndpoint(self):
+        return self.__endpoint_address
+
+    def Disconnect(self):
+        if self.__connected == False:
+            return True
+
+        self.__endpoint.close()
+        self.__endpoint = None
+        self.__connected = False
+        self.__sock.close()
+        self.__sock = None
     
+    def IsConnected(self):
+        return self.__connected
+    
+    def Send(self, message):
+        if self.__connected == False:
+            return SOCKET_ERROR_CONNECTION
+
+        self.__endpoint.sendall(message)
+
+    def Recv(self):
+        msg = None
+        readable = select.select([self.__endpoint],[], [],0.0) 
+       
+        if readable:
+            msg = self.__endpoint.recv(512)
+            if len(msg) == 0:
+                print "endpoint disconnected"
+                return None
+            else:   
+                return msg
+
+
+if __name__ == '__main__': 
+    import sys
+    from time import sleep
+    IP = "127.0.0.1"
+    PORT = 8000
+    server = Server()
+   
+
+    if server.Bind(IP, PORT):
+        print "Wait for connection at %s:%s" % (IP, PORT)
+    else:
+        print "Cannot bind at %s:%s" % (IP, PORT)
+
+    while not server.Accept():
+        sleep(1)
+
+    endpoint = server.GetEndpoint()
+    print "Endpoint connected from %s:%s" % (endpoint[0], endpoint[1])
+
+    while True:
+        #if not server.IsConnected():
+        #    print "Endpoint disconnected"
+        #    server.Accept()
+        
+        data = server.Recv()
+        if data:
+            print "Message: %s" % (data)
+
+    server.Disconnect()
+   
